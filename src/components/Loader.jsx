f5ad397bcc326798236e2a8b2c4d55d78402e62d
vutii <giftmuuo.com@gmail.com>
2026-04-07 11:16:28 +0300

    update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../css/Loader.css";
-import logo from "../assets/logo.png.jpg";
+import logo from "../assets/sacredlogo.png";
 
 const Loader = () => {
   return (
     <div className="loader-wrapper">
       <img src={logo} alt="logo" className="pulse-logo" />
-      <p className="loader-text">Corteiz Thrifters...</p>
+      <p className="loader-text">Sacred Vanity...</p>
     </div>
   );
 };
